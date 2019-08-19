22ab1c4d0a1ccd76beb4efbb012b92412ff03ce1
fanny <fanny.vieira@ccc.ufcg.edu.br>
2019-08-19 08:19:55 -0300

    Finish annotations and metadata extractions

--- genius/genius/spiders/genius.py
+++ genius/genius/spiders/genius.py
@@ -15,11 +15,22 @@ class GeniusSpider(scrapy.Spider):
             yield scrapy.Request(url=url, callback=self.parse_2)
 
     def parse_2(self, response):
-        page = response.url.split("/")[-1]
         song_metadata = response.css("div.rich_text_formatting p::text").getall()
         annotations = response.css("a::attr(annotation-fragment)").getall()
-        print(annotations)
+        for url in annotations:
+            link = 'https://genius.com/' + url
+            yield scrapy.Request(url=link, callback=self.parse_3)
+
+
+    def parse_3(self, response):
+        page = response.url.split("/")[-1]
         filename = 'quotes-%s.html' % page
+        snippet_lyric = response.css("meta[property='og:title']::attr(content)").getall()
+        annotation = response.css("meta[property='og:description']::attr(content)").getall()
+        print('Hey ------')
+        print(snippet_lyric)
+        print('\n\n ----- \n\n')
+        print(annotation)
         with open(filename, 'wb') as f:
             f.write(response.body)
         self.log('Saved file %s' % filename)
